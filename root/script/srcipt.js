be9b4d6b3128610783b8dffdab2e690a720c9344
Jorge Rivera <jrivera@VESSELARG.local>
2023-02-27 22:56:38 -0300

    envio de formulario con form submit

--- root/script/srcipt.js
+++ root/script/srcipt.js
@@ -1,24 +1,24 @@
-const navToggle = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".nav-menu");
+// const navToggle = document.querySelector(".nav-toggle");
+// const navMenu = document.querySelector(".nav-menu");
 
-navToggle.addEventListener("click", () => {
-  navMenu.classList.toggle("nav-menu_visible");
+// navToggle.addEventListener("click", () => {
+//   navMenu.classList.toggle("nav-menu_visible");
 
-  if (navMenu.classList.contains("nav-menu_visible")) {
-    navToggle.setAttribute("aria-label", "Cerrar menú");
-  } else {
-    navToggle.setAttribute("aria-label", "Abrir menú");
-  }
-});
-//efecto de scroll en header
-window.addEventListener('scroll', function() {
-  var header = document.querySelector('header');
-  if (window.scrollY >= 100) {
-    header.classList.add('small');
-  } else {
-    header.classList.remove('small');
-  }
-});
+//   if (navMenu.classList.contains("nav-menu_visible")) {
+//     navToggle.setAttribute("aria-label", "Cerrar menú");
+//   } else {
+//     navToggle.setAttribute("aria-label", "Abrir menú");
+//   }
+// });
+// //efecto de scroll en header
+// window.addEventListener('scroll', function() {
+//   var header = document.querySelector('header');
+//   if (window.scrollY >= 100) {
+//     header.classList.add('small');
+//   } else {
+//     header.classList.remove('small');
+//   }
+// });
 
 
 // Script Jorge
@@ -31,6 +31,19 @@ let clearBtn = document.getElementById("clearBtn");
 sendBtn.addEventListener("click", (e) => {
     e.preventDefault();
     console.log("boton Enviar Presionado")
+
+
+  const name = document.getElementById("inputNombre").value;
+  const message = document.getElementById("inputMessage").value;
+
+  console.log(name);
+  console.log(message);
+
+
+
+
+
+
 });
 
 clearBtn.addEventListener("click", (e) => {
