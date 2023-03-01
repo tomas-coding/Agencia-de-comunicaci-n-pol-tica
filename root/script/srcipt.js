e1371adf2b542e0b4891ce3352e0a008bd57a8f4
Tomas Lovato <lovato.tomas94@gmail.com>
2023-03-01 15:09:13 -0300

    ajuste de background y posicion de sobre nosotros

--- root/script/srcipt.js
+++ root/script/srcipt.js
@@ -1,24 +1,24 @@
-// const navToggle = document.querySelector(".nav-toggle");
-// const navMenu = document.querySelector(".nav-menu");
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
 
-// navToggle.addEventListener("click", () => {
-//   navMenu.classList.toggle("nav-menu_visible");
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-menu_visible");
 
-//   if (navMenu.classList.contains("nav-menu_visible")) {
-//     navToggle.setAttribute("aria-label", "Cerrar menú");
-//   } else {
-//     navToggle.setAttribute("aria-label", "Abrir menú");
-//   }
-// });
-// //efecto de scroll en header
-// window.addEventListener('scroll', function() {
-//   var header = document.querySelector('header');
-//   if (window.scrollY >= 100) {
-//     header.classList.add('small');
-//   } else {
-//     header.classList.remove('small');
-//   }
-// });
+  if (navMenu.classList.contains("nav-menu_visible")) {
+    navToggle.setAttribute("aria-label", "Cerrar menú");
+  } else {
+    navToggle.setAttribute("aria-label", "Abrir menú");
+  }
+});
+//efecto de scroll en header
+window.addEventListener('scroll', function() {
+  var header = document.querySelector('header');
+  if (window.scrollY >= 100) {
+    header.classList.add('small');
+  } else {
+    header.classList.remove('small');
+  }
+});
 
 
 // Script Jorge
